89a600917cc15031186122a3adae5109ab0e5b77
Smriti Patel <80051436+smriti596@users.noreply.github.com>
2022-03-23 21:13:43 +0530

    Add files via upload

---
+++ CODECHEF/STARTERS31/GROUPASSGN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,i;
+	cin>>t;
+    
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    cout<<(2*n)-(x-1)<<endl;
+	}
+	return 0;
+}
