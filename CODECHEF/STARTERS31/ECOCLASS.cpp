d7d1b2bad7291c19e1b2899308dce024920b4491
Smriti Patel <80051436+smriti596@users.noreply.github.com>
2022-03-23 20:35:37 +0530

    Add files via upload

---
+++ CODECHEF/STARTERS31/ECOCLASS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int s[n];
+	    int d[n];
+	    for(int i=0;i<n;i++)
+	        cin>>s[i];
+	    for(int i=0;i<n;i++)
+	        cin>>d[i];
+	    int count=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(s[i]==d[i])
+	            count++;
+	        
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
